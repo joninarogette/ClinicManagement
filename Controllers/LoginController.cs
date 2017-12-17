d27d29129a68ea55f3b21a6d5485b7894b2b2b7f
Sean Garcia <15.seangarcia@gmail.com>
2017-12-17 22:23:00 +0800

    additional login codes

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ClinicManagement.Models;
 
 namespace ClinicManagement.Controllers
 {
@@ -10,6 +11,13 @@ namespace ClinicManagement.Controllers
     {
         // GET: Login
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "StaffID, StaffPassword")] Staff staff)
         {
             return View();
         }
